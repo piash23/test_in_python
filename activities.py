f6bc1121febc47ff2ff16287c2642021d569fefe
Md Shihab Uddin <pythonpiash@gmial.com>
2025-08-03 23:37:02 +0600

    Add initial implementation of eat and nap functions with unit tests

---
+++ activities.py
@@ -0,0 +1,13 @@
+def eat(food, is_healthy):
+    ending = "because YOLO!"
+    if is_healthy:
+        ending = "because my body is a temple"
+
+    return f"I'm eating {food}, {ending}"
+
+def nap(duration):
+    hour_label = "hour" if duration == 1 else "hours"
+    if duration < 2:
+        return f"I'm feeling refreshed after {duration} {hour_label} of sleep."
+    else:
+        return f"Ugh I overslept. I didn't mean to nap for {duration} {hour_label}!"
